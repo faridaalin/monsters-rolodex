970f77c982c69321a2379dbb4b2e7bd17dab76f7
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-06-30 15:00:42 +0200

    Updates

---
+++ precache-manifest.f8b74067b730fa914a96b0d5cac9a195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa709c3ef6c0b9479707e56ad07076e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "285461ecd9d11545f568",
+    "url": "/monsters-rolodex/static/css/main.94cf46d4.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "285461ecd9d11545f568",
+    "url": "/monsters-rolodex/static/js/main.27520b27.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
